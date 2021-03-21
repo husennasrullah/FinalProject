0b68f3888f74b90a95d7b080592779b8b55c787b
husen <nasrullahhusen99@gmail.com>
2021-03-21 21:33:38 +0700

    minggu-21-maret-2021

--- src/main/java/com/gromart/springboot/service/OrderService.java
+++ src/main/java/com/gromart/springboot/service/OrderService.java
@@ -1,4 +1,23 @@
 package com.gromart.springboot.service;
 
+import com.gromart.springboot.model.Cart;
+import com.gromart.springboot.model.CartDetail;
+import com.gromart.springboot.model.Order;
+
+import java.util.List;
+
 public interface OrderService {
+    List<Order> findAllOrders();
+
+    void saveOrder (Order order);
+
+    Order findByUserId (String userId);
+
+    Order findById (String orderId);
+
+    void deleteOrderById(String orderId);
+
+    int countDetail (String cartId);
+
+    void updateStatus (Order order);
 }
--- src/main/java/com/gromart/springboot/service/OrderServiceImpl.java
+++ src/main/java/com/gromart/springboot/service/OrderServiceImpl.java
@@ -1,5 +1,43 @@
 package com.gromart.springboot.service;
 
-public class OrderServiceImpl {
+import com.gromart.springboot.model.Order;
 
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Override
+    public List<Order> findAllOrders() {
+        return null;
+    }
+
+    @Override
+    public void saveOrder(Order order) {
+
+    }
+
+    @Override
+    public Order findByUserId(String userId) {
+        return null;
+    }
+
+    @Override
+    public Order findById(String orderId) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrderById(String orderId) {
+
+    }
+
+    @Override
+    public int countDetail(String cartId) {
+        return 0;
+    }
+
+    @Override
+    public void updateStatus(Order order) {
+
+    }
 }
